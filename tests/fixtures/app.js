f83b35060789d15baea39f2e6daf3890dc2c8f6d
AlisonV2 <alison.vandromme007@gmail.com>
2022-03-04 11:09:55 +0100

    fixed

--- tests/fixtures/app.js
+++ tests/fixtures/app.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import Server from '../../src/config/Server';
 import { usersRouter } from '../../src/routes/users';
@@ -7,12 +6,13 @@ import { tokensRouter } from '../../src/routes/tokens';
 import { sessionsRouter } from '../../src/routes/sessions';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(Server.setHeaders);
-app.use('/api/users', usersRouter);
-app.use('/api/tasks', tasksRouter);
-app.use('/api/tokens', tokensRouter);
-app.use('/api/sessions', sessionsRouter);
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(Server.setHeaders)
+  .use('/api/users', usersRouter)
+  .use('/api/tasks', tasksRouter)
+  .use('/api/tokens', tokensRouter)
+  .use('/api/sessions', sessionsRouter);
 
 export default app;
